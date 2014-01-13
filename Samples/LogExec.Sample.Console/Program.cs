cbfb9c59d2519ad6f542a741e9680054eebd1d4e
chai-deshpande <chaitanya@kashflow.com>
2014-01-14 01:41:10 +0530

    Added sample console application

---
+++ Samples/LogExec.Sample.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+
+namespace LogExec.Sample.Console
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      using (new ExecutionTimeLogger("Console Sample"))
+      {
+        Thread.Sleep(1500);
+      }
+    }
+  }
+}
