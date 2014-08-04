0de47a3ef2add9f68de38f87179ec92cfffd97c7
Chai Deshpande <chaitanya@kashflow.com>
2014-08-04 23:05:02 +0100

    1. Added ability to specify thresholds for warning, error and fatal log messages 2. This adds the ability to do various things like e.g. add an event viewer entry if the execution time goes above a certain level and send emails if it is in excess of yet another level

--- Samples/LogExec.Sample.Console/Program.cs
+++ Samples/LogExec.Sample.Console/Program.cs
@@ -7,7 +7,7 @@ namespace LogExec.Sample.Console
   {
     static void Main(string[] args)
     {
-      var executionTimeLogger = new ExecutionTimeLogger("Main function");
+      var executionTimeLogger = new ExecutionTimeLogger("Main function", true, false);
 
       // Enclose the code you are interested in a using block.
       using (var workLogger = new ExecutionTimeLogger("Dummy Work"))
@@ -15,22 +15,43 @@ namespace LogExec.Sample.Console
         // Do some useful work.
 
         // Pretending to do useful work.
-        Thread.Sleep(1500);
+        Thread.Sleep(50);
         workLogger.LogMilestone("DAL completed");
 
         // Pretending to more useful work.
-        Thread.Sleep(400);
+        Thread.Sleep(10);
         workLogger.LogMilestone("Business logic completed");
+
+        // Useful work done.
+        Thread.Sleep(10);
+      }
+      executionTimeLogger.LogMilestone("Finished dummy work");
+
+      Thread.Sleep(40);
+
+      executionTimeLogger.Stop();
+      executionTimeLogger.Log();
+
+      // Enclose the code you are interested in a using block including custom logging types
+      using (var workLogger = new ExecutionTimeLogger("Dummy Work with logging types", true, false))
+      {
+        // Do some useful work.
+
+        // Pretending to do useful work.
+        Thread.Sleep(50);
+        workLogger.LogMilestone("CUSTOM - DAL completed");
+
+        // Pretending to more useful work.
+        Thread.Sleep(400);
+        workLogger.LogMilestone("CUSTOM - Business logic completed");
+
+        // Pretending to more useful work.
+        Thread.Sleep(400);
+        workLogger.LogMilestone("CUSTOM - External component business logic completed");
 
         // Useful work done.
         Thread.Sleep(100);
       }
-      executionTimeLogger.LogMilestone("Finished dummy work");
-
-      Thread.Sleep(200);
-
-      executionTimeLogger.Stop();
-      executionTimeLogger.Log();
     }
   }
 }
